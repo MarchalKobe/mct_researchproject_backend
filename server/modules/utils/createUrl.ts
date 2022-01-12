1644a2a4c31341818da1fb8d595661f84ae10f46
MarchalKobe <kobe.marchal@student.howest.be>
2022-01-12 23:54:45 +0100

    Added styling for register, login and confirm screen

--- server/modules/utils/createUrl.ts
+++ server/modules/utils/createUrl.ts
@@ -8,8 +8,8 @@ export const createConfirmationUrl = async (userId: string) => {
     return `http://localhost:3001/confirm?token=${token}`;
 };
 
-export const createPasswordRestoreUrl = async (email: string) => {
-    const token = v4();
-    await redis.set(`passwordrestore-${token}`, email, 'ex', 60 * 60 * 4) // 4 hours experation
-    return `http://localhost:3001/wachtwoordherstellen?token=${token}`;
-};
+// export const createPasswordRestoreUrl = async (email: string) => {
+//     const token = v4();
+//     await redis.set(`passwordrestore-${token}`, email, 'ex', 60 * 60 * 4) // 4 hours experation
+//     return `http://localhost:3001/wachtwoordherstellen?token=${token}`;
+// };
