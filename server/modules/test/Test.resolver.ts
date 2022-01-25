ace2fc55ea0cc91afdf08067fb499c066d7b9afe
MarchalKobe <kobe.marchal@student.howest.be>
2022-01-25 20:18:34 +0100

    Update score

--- server/modules/test/Test.resolver.ts
+++ server/modules/test/Test.resolver.ts
@@ -1,29 +1,26 @@
 import { Query, Resolver } from 'type-graphql';
-import jsdom from 'jsdom';
 import levenshtein from 'fast-levenshtein';
 
-
-const html1 = `
+const student = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <title>Headers and paragraphs</title>
     </head>
     <body>
-        <h1>This is the main heading of the page</h1>
-        <input type="text" id="test" name="test" required />
+        <h1>This is the main heading the page</h1>
+        <input type="text" id="test" required="required" />
         <p>h1 defines the most important heading of the page.</p>
         <h2>This is the first sub-heading</h2>
         <p>h2 describes the first sub-heading of the page.</p>
-        <h3>This is the second sub-heading</h3>
+        <h2>This is the second sub-heading</h2>
         <p>h3 describes the second sub-heading of the page.</p>
         <p>The h1 to h6 tags are used to define HTML headings.</p>
     </body>
     </html>
 `;
 
-
-const html2 = `
+const assignment = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -43,7 +40,6 @@ const html2 = `
 `;
 
 
-
 const isValidHTML = async (html: string) => {
     const validator = require('html-validator');
 
@@ -58,11 +54,67 @@ const isValidHTML = async (html: string) => {
 };
 
 
-const makePercentage = (x: number) => {
-    // 0 mistakes = 100%
-    // 5 mistakes = 50% 
-    return -2 * Math.pow(x, 2) + 0 * x + 100;
+interface Categories {
+    code: string;
+    tags: string;
+    attributes: string;
+    text: string;
 };
+
+interface Distance {
+    tags: number;
+    attributes: number;
+    text: number;
+};
+
+
+const divideCategories = (code: string) => {
+    const removeAttributes = /<\s*([a-z][a-z0-9]*)\s.*?>/gi;
+    const getText = /<\/?[^>]+(>|$)/g;
+    const removeTags = /\<\w*|>/g;
+    const removeSpaces = / +(?= )/g;
+
+    const result: Categories = {
+        code: '',
+        tags: '',
+        attributes: '',
+        text: '',
+    };
+    
+    // Remove attributes
+    result.tags = code.replace(removeAttributes, '<$1>');
+    result.tags = result.tags.match(getText)!.join('');
+
+    // Get attributes
+    result.attributes = code.match(removeAttributes)!.join('').replace(removeTags, '').replace(removeSpaces, '');
+
+    // Get text
+    result.text = code.replace(getText, ' ').replace(removeSpaces, '');
+
+    return result;
+};
+
+
+const calculateLevenshteinDistance = (result1: Categories, result2: Categories) => {
+    const distance: Distance = {
+        tags: 0,
+        attributes: 0,
+        text: 0,
+    };
+
+    distance.tags = levenshtein.get(result1.tags, result2.tags);
+    distance.attributes = levenshtein.get(result1.attributes, result2.attributes);
+    distance.text = levenshtein.get(result1.text, result2.text);
+
+    return distance;
+};
+
+
+// const makePercentage = (x: number) => {
+//     // 0 mistakes = 100%
+//     // 5 mistakes = 50% 
+//     return -2 * Math.pow(x, 2) + 0 * x + 100;
+// };
 
 
 @Resolver()
@@ -73,7 +125,7 @@ export class TestResolver {
     async test(): Promise<Boolean | undefined | null> {
         try {
             // Check if no errors and valid html file
-            const isValid = await isValidHTML(html1);
+            const isValid = await isValidHTML(student);
 
             if(isValid) {
                 const minify = require('html-minifier').minify;
@@ -85,41 +137,21 @@ export class TestResolver {
                     sortAttributes: true,
                 };
 
-                const result = minify(html1, options);
+                const assignmentMinify = minify(assignment, options);
+                const studentMinify = minify(student, options);
 
-                // const result2 = minify(html2, options);
+                const resultAssignment = divideCategories(assignmentMinify);
+                const resultStudent = divideCategories(studentMinify);
 
-                // console.log('Assignment:', result);
-                // console.log('Your code:', result2);
+                resultAssignment.code = assignment;
+                resultStudent.code = student;
 
+                const distance = calculateLevenshteinDistance(resultAssignment, resultStudent);
 
-
-                const removeAttributes = /<\s*([a-z][a-z0-9]*)\s.*?>/gi;
-                const getText = /<\/?[^>]+(>|$)/g;
-                const getAttributes = /(\S+)\s*=\s*([']|["])\s*([\W\w]*?)\s*\2/g;
+                console.log('Student:', resultStudent);
+                console.log('Assignment:', resultAssignment);
+                console.log('Levenshtein distance:', distance);
                 
-                // Remove attributes
-                let tags = result.replace(removeAttributes, '<$1>');
-                tags = tags.match(getText).join('');
-                console.log(tags);
-
-                // Get text
-                const text = result.replace(getText, ' ').replace(/ +(?= )/g, '');
-                console.log(text);
-
-                // Get attributes
-                const attributes = result.match(getAttributes).join(' ');
-                console.log(attributes);
-
-                
-
-                
-                // const distance = levenshtein.get(result, result2);
-
-                // console.log('Levenshtein distance:', distance);
-                
-                // console.log(`Score: ${makePercentage(distance)}%`);
-
                 return true;
             } else {
                 // Give 0 score
